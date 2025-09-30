60120e84f3f8d48efb635e600d483d70f6e4d51d
mikfairo <mikfairo123@gmail.com>
2025-09-30 00:30:25 -0400

    Added header and fixed styling in css

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 async function initialLoad() {
-    const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
-    const data = await response.json();
-    console.log(data);
+  const bkgDiv = document.getElementById("background-div");
+  bkgDiv.style.backgroundImage =
+    "url(https://images.unsplash.com/photo-1614548539924-5c1f205b3747?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
+
+  const topHeader = document.createElement("h1");
+  topHeader.textContent= ("Whats for dinner?");
+  bkgDiv.appendChild(topHeader);
+  const response = await fetch(   
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+  const data = await response.json();
+  console.log(data);
+  const thumbnail = `url(${data.meals[0].strMealThumb})`;
+  console.log(thumbnail);
+  const dishDiv = document.createElement("div");
+  dishDiv.id = "dish-div";
+  dishDiv.style.backgroundImage = thumbnail; //to add the photo from link to the dish div 
+  bkgDiv.appendChild(dishDiv); 
+  // return thumbnail;
 }
 initialLoad();
-const bkgDiv = document.getElementById("background-div");
-bkgDiv.style.backgroundImage= "url(https://images.unsplash.com/photo-1614548539924-5c1f205b3747?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
 
-const dishDiv = document.createElement("div")
-dishDiv.id = "dish-div"
-bkgDiv.appendChild(dishDiv);
+// const thumbnail = await initialLoad();
+// const bkgDiv = document.getElementById("background-div");
+// bkgDiv.style.backgroundImage= "url(https://images.unsplash.com/photo-1614548539924-5c1f205b3747?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
+
+// const dishDiv = document.createElement("div")
+// dishDiv.id = "dish-div"
+// dishDiv.style.backgroundImage = (thumbnail);
+// bkgDiv.appendChild(dishDiv);
