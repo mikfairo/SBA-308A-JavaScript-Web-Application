b5d05c96eaa9740a0fd2991b0628c40f89c87d59
mikfairo <mikfairo123@gmail.com>
2025-09-30 21:33:26 -0400

    Added a second module file for drink div and added a tablecloth background. Tried to figure out placement

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 async function initialLoad() {
-  const bkgDiv = document.getElementById("background-div");
-  bkgDiv.style.backgroundImage =
+  const tableDiv = document.getElementById("table-div");
+  tableDiv.style.backgroundImage =
     "url(https://images.unsplash.com/photo-1614548539924-5c1f205b3747?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
 
   const topHeader = document.createElement("h1");
   topHeader.textContent= ("Whats for dinner?");
-  bkgDiv.appendChild(topHeader);
+  tableDiv.appendChild(topHeader);
   const response = await fetch(   
     "https://www.themealdb.com/api/json/v1/1/random.php"
   );
   const data = await response.json();
   console.log(data);
-  const thumbnail = `url(${data.meals[0].strMealThumb})`;
-  console.log(thumbnail);
+  const firstThumbnail = `url(${data.meals[0].strMealThumb})`;
+  console.log(firstThumbnail);
   const dishDiv = document.createElement("div");
   dishDiv.id = "dish-div";
-  dishDiv.style.backgroundImage = thumbnail; //to add the photo from link to the dish div 
-  bkgDiv.appendChild(dishDiv); 
+  dishDiv.style.backgroundImage = firstThumbnail; //to add the photo from link to the dish div 
+  tableDiv.appendChild(dishDiv); 
   // return thumbnail;
 }
 initialLoad();
+secondaryLoad();
 
 // const thumbnail = await initialLoad();
 // const bkgDiv = document.getElementById("background-div");
---
+++ second.js
@@ -0,0 +1,17 @@
+async function secondaryLoad() {
+    const bkgDiv = document.getElementById("background-div");
+  bkgDiv.style.backgroundImage =
+    "url(https://images.unsplash.com/photo-1600294421265-c354b772e790?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
+
+const response = await fetch(   
+    "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+  );
+  const data = await response.json();
+  console.log(data);
+    const secondThumbnail = `url(${data.drinks[0].strDrinkThumb})`;
+  console.log(secondThumbnail);
+  const drinkDiv = document.createElement("div");
+  drinkDiv.id = "drink-div";
+  drinkDiv.style.backgroundImage = secondThumbnail;  
+  bkgDiv.appendChild(drinkDiv); 
+}
